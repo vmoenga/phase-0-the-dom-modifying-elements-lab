56418a7d8bc332a3362af9cbe03c0f0061d89887
Victor <vmoenga36@gmail.com>
2024-04-16 15:21:16 +0300

    add new

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const input = document.querySelector('input')
+function addingEventListener(){
+    input .addEventListener('click', function() {
+        alert('I was clicked!');
+      });
+}
